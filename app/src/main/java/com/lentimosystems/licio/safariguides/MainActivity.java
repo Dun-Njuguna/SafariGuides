a782a1093380740ef5c33968b1570bceee0f86a5
liciolentimo <lentimo@gmail.com>
2019-05-31 13:33:20 +0300

    Added login feature

--- app/src/main/java/com/lentimosystems/licio/safariguides/MainActivity.java
+++ app/src/main/java/com/lentimosystems/licio/safariguides/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+                Intent homeIntent = new Intent(MainActivity.this,CreateAccountActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
